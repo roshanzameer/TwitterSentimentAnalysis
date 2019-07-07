4dc16d16ab210b9f93e44dbde2efffee8a3a4b3c
Roshan Zameer <rossi.zameer@gmail.com>
2019-07-07 15:32:35 +0530

    Update credentials.py

--- Producer/credentials.py
+++ Producer/credentials.py
@@ -4,10 +4,10 @@ The global variables Consumer keys, Secrets are to be set accordingly
 
 """
 
-consumer_key = 'Xfgl5IvWbP6PDXsppSpJjwYrm'
+consumer_key = 'XXXXXXXXXX'
 
-consumer_secret = 'KMsNoi5R78vh0NboBW8dG3subkmlq44T5TygR1PUpJQtyVcUH8'
+consumer_secret = 'XXXXXXXXXX'
 
-access_token = '2544128711-NEtSgtGFmtwywkYHlF8FBVtUcQynmCccygavvEp'
+access_token = 'XXXXXXXXXX-XXXXXXXXXX'
 
-access_secret = 'ZpfVx5l2JGowGlNc21tyDnN2Z4pJck2fXfN4gKDmL2Lw2'
+access_secret = 'XXXXXXXXXX'
