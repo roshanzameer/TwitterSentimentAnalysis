939f95aac40eeb7c1edbe57a3718aeba57d7a8cd
Roshan Zameer <rossi.zameer@gmail.com>
2019-07-07 15:36:40 +0530

    Create credentials.py

---
+++ Producer/credentials.py
@@ -0,0 +1,13 @@
+"""
+
+Consumer keys, Secrets are to be set accordingly
+
+"""
+
+consumer_key = 'XXXXXXXXXX'
+
+consumer_secret = 'XXXXXXXXXX'
+
+access_token = 'XXXXXXXXXX-XXXXXXXXXX'
+
+access_secret = 'XXXXXXXXXX'
